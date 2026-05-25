51ea9c4f53c1b467c55ef67a24550aa04d821b2b
Nolan DESTANG <nolandestang@gmail.com>
2026-05-25 15:11:55 +0200

    first commit

---
+++ ressources/script.js
@@ -0,0 +1,27 @@
+// animation des images des cours : le texte de la section s'affiche au clic sur chaque image
+// Sélection des éléments du DOM
+const imagesCours = document.querySelectorAll('.presentation_sections img');
+const textesCours = document.querySelectorAll('.texte_cours');
+const boutonsPlusInfos = document.querySelectorAll('.plus_infos');
+
+// temps en millisecondes avant l'apparition de chaque partie de la description après le clic
+const delayAffichageTexte = 250;
+const delayAffichageBouton = 750;
+
+//appel de la fonction pour afficher la description
+imagesCours.forEach((image, index) => {
+    image.addEventListener('click', () => afficherDescriptionCours(index));
+});
+
+//fonction pour afficher la description d'un cours
+function afficherDescriptionCours(index) {
+    if (!textesCours[index]) return;
+
+    setTimeout(() => {
+        textesCours[index].style.opacity = 1;
+    }, delayAffichageTexte);
+
+    setTimeout(() => {
+        boutonsPlusInfos[index].style.opacity = 1;
+    }, delayAffichageBouton);
+};
